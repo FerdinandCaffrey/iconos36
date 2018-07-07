1418d6dcb07e6878f3821c2250ee8b214acc2a3f
Jonathan MirCha <jonmircha@gmail.com>
2018-07-07 14:43:09 -0500

    Versión 1 del tema WP

---
+++ WordPress/kenai_v1/index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Layout MedioTiempo.com</title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header class="Header">
+    <section class="Header-container">
+      <div class="Logo">
+        <a href="#">
+          <img src="https://fantastica.mediotiempo.com/assets/images/logo-black.png">
+        </a>
+      </div>
+      <button class="Panel-btn  hamburger  hamburger--squeeze" type="button">
+        <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+        </span>
+      </button>
+      <section class="Panel">
+        <nav class="Menu">
+          <ul>
+            <li>
+              <a href="">Sección 1</a>
+            </li>
+            <li>
+              <a href="">Sección 2</a>
+            </li>
+            <li>
+              <a href="">Sección 3</a>
+            </li>
+            <li>
+              <a href="">Sección 4</a>
+            </li>
+            <li>
+              <a href="">Sección 5</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </section>
+  </header>
+  <article class="LatestNews  u-afterFixed">
+    <div class="LatestNews-container">
+      <a href="#" class="u-latest">
+        <img src="https://mt.brightspotcdn.com/dims4/default/6ce1bce/2147483647/thumbnail/800x450%5E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2Fdc%2F83%2Fd9c809f24b33994df0123f42aa92%2Fmex-vs-kor-top-1.jpg">
+      </a>
+      <a href="#">
+        <img src="https://mt.brightspotcdn.com/dims4/default/7f1e542/2147483647/thumbnail/400x225%5E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F77%2F2b%2F76d4f5a54f87933c60d54f7447bf%2Ftopalemaniaconletras.jpg">
+      </a>
+      <a href="#">
+        <img src="https://mt.brightspotcdn.com/dims4/default/584322e/2147483647/thumbnail/400x225%5E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2Fec%2F2f%2F8ad09c6243709e706c0eb709e101%2Ftop-gol-ch14.jpg ">
+      </a>
+    </div>
+  </article>
+  <main class="Main">
+    <div class="Main-container">
+      <section class="TheLast">
+        <h2 class="Main-title">Lo Último</h2>
+        <article class="Card-main">
+          <a href="#">
+            <h5>Selección Mexicana</h5>
+            <img src="https://mt.brightspotcdn.com/dims4/default/d117e1f/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F44%2F96%2F6ad4813f4a24853ef011fa00004f%2Ftri.jpg">
+            <h3>México no hace cálculos sobre posiciones o resultados para evitar a Brasil</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/9c650fc/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F9d%2Fcc%2Fa0abf11a45f3a1625950b17b335f%2F1.jpg">
+            <h3>Ochoa disfrutó más el triunfo contra Corea por vivirlo en familia</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/9c650fc/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F9d%2Fcc%2Fa0abf11a45f3a1625950b17b335f%2F1.jpg">
+            <h3>Ochoa disfrutó más el triunfo contra Corea por vivirlo en familia</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/9c650fc/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F9d%2Fcc%2Fa0abf11a45f3a1625950b17b335f%2F1.jpg">
+            <h3>Ochoa disfrutó más el triunfo contra Corea por vivirlo en familia</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/9c650fc/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F9d%2Fcc%2Fa0abf11a45f3a1625950b17b335f%2F1.jpg">
+            <h3>Ochoa disfrutó más el triunfo contra Corea por vivirlo en familia</h3>
+          </a>
+        </article>
+      </section>
+      <section class="Recommended">
+        <h2 class="Main-title">Recomendado</h2>
+        <article class="Card-main">
+          <a href="#">
+            <h5>Selección Mexicana</h5>
+            <img src="https://mt.brightspotcdn.com/dims4/default/9c650fc/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F9d%2Fcc%2Fa0abf11a45f3a1625950b17b335f%2F1.jpg">
+            <h3>Ochoa disfrutó más el triunfo contra Corea por vivirlo en familia</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/d117e1f/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F44%2F96%2F6ad4813f4a24853ef011fa00004f%2Ftri.jpg">
+            <h3>México no hace cálculos sobre posiciones o resultados para evitar a Brasil</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/d117e1f/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F44%2F96%2F6ad4813f4a24853ef011fa00004f%2Ftri.jpg">
+            <h3>México no hace cálculos sobre posiciones o resultados para evitar a Brasil</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/d117e1f/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F44%2F96%2F6ad4813f4a24853ef011fa00004f%2Ftri.jpg">
+            <h3>México no hace cálculos sobre posiciones o resultados para evitar a Brasil</h3>
+          </a>
+        </article>
+        <article class="Card-second">
+          <a href="#">
+            <div>
+              <h5>Selección Mexicana</h5>
+            </div>
+            <img src="https://mt.brightspotcdn.com/dims4/default/d117e1f/2147483647/resize/390x%3E/quality/90/?url=https%3A%2F%2Fmt.brightspotcdn.com%2F44%2F96%2F6ad4813f4a24853ef011fa00004f%2Ftri.jpg">
+            <h3>México no hace cálculos sobre posiciones o resultados para evitar a Brasil</h3>
+          </a>
+        </article>
+      </section>
+      <section class="Calendar">
+        <h2 class="Main-title">Calendario</h2>
+        <h3 class="Match-title">Sáb 23 de Junio, 2018</h3>
+        <article class="Match">
+          <a href="#">
+            <p>5</p>
+            <figure class="Country">
+              <img src="https://static.mediotiempo.com/0x60/bs/mt/sports/production/soccer/teams/d6f90d69-9030-42e5-9629-e8ac1052d07a.png">
+              <figcaption>Bélgica</figcaption>
+            </figure>
+            <span>Resumen</span>
+            <figure class="Country">
+              <img src="https://static.mediotiempo.com/0x60/bs/mt/sports/production/soccer/teams/b50b10b5-fb0d-40b8-a100-707a44120983.png">
+              <figcaption>Túnez</figcaption>
+            </figure>
+            <p>2</p>
+          </a>
+        </article>
+        <article class="Match">
+          <a href="#">
+            <p>1</p>
+            <figure class="Country">
+              <img src="https://static.mediotiempo.com/0x60/bs/mt/sports/production/soccer/teams/bcc5dbfa-36f9-4534-bf16-50e7806c8382.png">
+              <figcaption>Corea del Sur</figcaption>
+            </figure>
+            <span>Resumen</span>
+            <figure class="Country">
+              <img src="https://static.mediotiempo.com/0x60/bs/mt/sports/production/soccer/teams/dfffd75b-df5c-413a-bf3b-e292a89fc650.png">
+              <figcaption>México</figcaption>
+            </figure>
+            <p>2</p>
+          </a>
+        </article>
+        <article class="Match">
+          <a href="#">
+            <p>5</p>
+            <figure class="Country">
+              <img src="https://static.mediotiempo.com/0x60/bs/mt/sports/production/soccer/teams/d6f90d69-9030-42e5-9629-e8ac1052d07a.png">
+              <figcaption>Bélgica</figcaption>
+            </figure>
+            <span>Resumen</span>
+            <figure class="Country">
+              <img src="https://static.mediotiempo.com/0x60/bs/mt/sports/production/soccer/teams/b50b10b5-fb0d-40b8-a100-707a44120983.png">
+              <figcaption>Túnez</figcaption>
+            </figure>
+            <p>2</p>
+          </a>
+        </article>
+      </section>
+    </div>
+  </main>
+  <footer class="Footer ">
+    <section class="Footer-container ">
+      <div class="Footer-nav ">
+        <nav class="SocialMedia ">
+          <a href="https://facebook.com " target="_blank ">
+            <i class="fab fa-facebook "></i>
+          </a>
+          <a href="https://twitter.com " target="_blank ">
+            <i class="fab fa-twitter "></i>
+          </a>
+          <a href="https://github.com/jonmircha/iconos36 " target="_blank ">
+            <i class="fab fa-github "></i>
+          </a>
+          <a href="https://youtube.com " target="_blank ">
+            <i class="fab fa-youtube "></i>
+          </a>
+          <a href="https://instagram.com " target="_blank ">
+            <i class="fab fa-instagram "></i>
+          </a>
+        </nav>
+      </div>
+      <div class="Footer-copy ">
+        <p>
+          &copy; 2018 Copyright
+          <a href="https://jonmircha.com " target="_blank ">@jonmircha</a>
+        </p>
+      </div>
+    </section>
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+
+</html>
